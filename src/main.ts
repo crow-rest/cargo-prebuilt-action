0329d4567e481b127739d2e71ad4332cabbbe6c6
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2023-03-06 14:22:38 -0500

    Forgot to do last element of array

--- src/main.ts
+++ src/main.ts
@@ -43,7 +43,7 @@ async function run(): Promise<void> {
           return 1
         return -1
       })
-      prebuiltVersion = latest[0]
+      prebuiltVersion = latest[latest.length - 1]
     }
     if (prebuiltTarget === 'current') {
       prebuiltTarget = await currentTarget()
